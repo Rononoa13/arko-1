6cd76993c11d1877630d11daccb23f1e599f2fa1
Sumit Luitel <sltel@Sumits-MacBook-Pro.local>
2026-09-25 13:08:32 +0545

    Add PWA manifest and service worker

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = "arko-1-v1";
+
+const APP_ASSETS = [
+    "/",
+    "/index.html",
+    "/css/app.css",
+    "/js/app.js",
+    "/manifest.json"
+];
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(APP_ASSETS))
+    );
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((cachedResponse) => {
+                return cachedResponse || fetch(event.request);
+            })
+    );
+});
